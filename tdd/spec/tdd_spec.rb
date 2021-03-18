38d0b42f54e062b1c96c9eac8bc77f629f9031f3
Daniel <dannyblaze469@gmail.com>
2021-03-18 14:36:30 -0400

    Created my_uniq in rspec

--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -2,12 +2,26 @@ require "tdd"
 require "rspec"
 
 describe Array do
-    describe "initialize" do
+    describe "#my_uniq" do
         subject(:array) {Array.new}
         it "remove duplicate elements in an array" do
             expect(array.my_uniq).to eq(array.uniq)
         end
     end
+
+    describe "#two_sum" do 
+        # array = [-1, 0, 2, -2, 1]
+        subject(:array) {[-1, 0, 2, -2, 1]}
+        it "return numbers that sum to zero" do 
+            
+        end
+        it "return pairs" do
+        end
+        it "should return sorted pairs" do 
+
+        end
+    end
+
 
 
 
