fe636dcb4b811a7a2aa688168e4aac1fcccad0f1
chrisweeting <chrisweeting@gmail.com>
2021-03-18 14:15:32 -0400

    Created test for #my_uniq

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -1,3 +1,13 @@
 class Array
-  
+    def my_uniq
+        new_arr = []
+
+        self.each do |ele|
+            unless new_arr.include?(ele)
+                new_arr << ele
+            end
+        end
+        
+        new_arr
+    end
 end
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -1,2 +1,15 @@
 require "tdd"
 require "rspec"
+
+describe Array do
+    describe "initialize" do
+        subject(:array) {Array.new}
+        it "remove duplicate elements in an array" do
+            expect(array.my_uniq).to eq(array.uniq)
+        end
+    end
+
+
+
+
+end
