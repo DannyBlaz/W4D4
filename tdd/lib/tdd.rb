ae9729be49cd024da9f512b03a1079c39e96eff9
Daniel <dannyblaze469@gmail.com>
2021-03-18 15:14:23 -0400

    Created specs for my_transpose

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -10,4 +10,15 @@ class Array
         
         new_arr
     end
+
+    def my_two_sum
+        new_arr = []
+        self.each_with_index do |num1, i|
+            self.each_with_index do |num2, j|
+                new_arr << [i, j] if j > i && num1 + num2 == 0
+            end
+        end
+        new_arr
+    end
+
 end
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -10,18 +10,24 @@ describe Array do
     end
 
     describe "#my_two_sum" do 
-        # array = [-1, 0, 2, -2, 1]
         subject(:array) {[-1, 0, 2, -2, 1]}
         it "return the indices of elements that sum to zero" do 
-            expect(:array.my_two_sum).to eq([[0, 4], [2, 3]])
+            expect(array.my_two_sum).to eq([[0, 4], [2, 3]])
         end
         it "return pairs" do
-            expect(:array.my_two_sum.all? {|ele| ele.length == 2}).to eq(true)
+            expect(array.my_two_sum.all? {|ele| ele.length == 2}).to eq(true)
         end
     end
 
-
-
+    describe "#my_transpose" do
+        subject(:array) {[[0, 1, 2], [3, 4, 5], [6, 7, 8]]} 
+        it "should convert between row oriented and column oriented orientation" do
+            expect(array.my_transpose).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+        end
+        it "should not mutate the array" do 
+            
+        end
+    end
 
 
 end
